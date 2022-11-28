b1515e1a4ed8db39af25c30f1ccc59ad4d84f020
Andrey Karmanov <Andrey@Karmanov.ca>
2022-11-28 18:41:28 -0500

    fix rotations

--- main.cc
+++ main.cc
@@ -13,14 +13,19 @@ int main(int argc, char *argv[])
     using namespace std;
     // Create pieces
     vector<vector<bool>> Igrid =
-        {{true, true, true, true}};
+        {{false, false, false, false},
+         {false, false, false, false},
+         {false, false, false, false},
+         {true, true, true, true}};
     Piece *Ipiece = new Piece(Igrid, 0, 0, 'I');
     vector<vector<bool>> Jgrid =
-        {{true, true, true},
+        {{false, false, false},
+         {true, true, true},
          {false, false, true}};
     Piece *Jpiece = new Piece(Jgrid, 0, 0, 'J');
     vector<vector<bool>> Lgrid =
-        {{true, true, true},
+        {{false, false, false},
+         {true, true, true},
          {true, false, false}};
     Piece *Lpiece = new Piece(Lgrid, 0, 0, 'L');
     vector<vector<bool>> Ogrid =
@@ -28,15 +33,18 @@ int main(int argc, char *argv[])
          {true, true}};
     Piece *Opiece = new Piece(Ogrid, 0, 0, 'O');
     vector<vector<bool>> Sgrid =
-        {{false, true, true},
+        {{false, false, false},
+         {false, true, true},
          {true, true, false}};
     Piece *Spiece = new Piece(Sgrid, 0, 0, 'S');
     vector<vector<bool>> Tgrid =
-        {{true, true, true},
+        {{false, false, false},
+         {true, true, true},
          {false, true, false}};
     Piece *Tpiece = new Piece(Tgrid, 0, 0, 'T');
     vector<vector<bool>> Zgrid =
-        {{true, true, false},
+        {{false, false, false},
+         {true, true, false},
          {false, true, true}};
     Piece *Zpiece = new Piece(Zgrid, 0, 0, 'Z');
     vector<Piece *> pieces = {Ipiece, Jpiece, Lpiece, Opiece, Spiece, Tpiece, Zpiece};
@@ -45,7 +53,7 @@ int main(int argc, char *argv[])
     Game *g = new Game("Andrey", pf, 10, 10);
     // Create a text observer
     TextObserver *to = new TextObserver(g);
-    g->setPiece('I');
+    g->setPiece('S');
     g->attach(to);
     g->notifyObservers();
     g->move(1, 0);
